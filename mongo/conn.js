9086616d9cacaa049786c9a6fb83257a201609ea
arulkumara1010 <arulkumara1010@gmail.com>
2025-02-04 13:40:21 +0530

    dbchange

--- mongo/conn.js
+++ mongo/conn.js
@@ -1,19 +1,19 @@
 import mongoose from "mongoose";
 
 const connectionString =
-  "mongodb+srv://yutira-admin:sNrmMV4MO18f9YFT@yutira-2025.t5adc.mongodb.net/?retryWrites=true&w=majority";
-
+    //"mongodb+srv://yutira-admin:sNrmMV4MO18f9YFT@yutira-2025.t5adc.mongodb.net/?retryWrites=true&w=majority";
+    "mongodb+srv://yutira-admin:yHSsegdj6EdTj9q3@cluster0.cthev.mongodb.net/?retryWrites=true&w=majority"
 export const connectToServer = (callback) => {
-  mongoose
-    .connect(connectionString, {})
-    .then(() => {
-      console.log("Connected to MongoDB");
-      return callback();
-    })
-    .catch((err) => {
-      console.log(err);
-      return callback(err);
-    });
+    mongoose
+        .connect(connectionString, {})
+        .then(() => {
+            console.log("Connected to MongoDB");
+            return callback();
+        })
+        .catch((err) => {
+            console.log(err);
+            return callback(err);
+        });
 };
 
 
