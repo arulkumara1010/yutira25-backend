a928c9658a8c21b82d859b2de9a93e0aa70d52ea
Arulkumara Babu <22z310@psgtech.ac.in>
2025-02-04 14:10:11 +0530

    Update conn.js

--- mongo/conn.js
+++ mongo/conn.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
 
 const connectionString =
-    //"mongodb+srv://yutira-admin:sNrmMV4MO18f9YFT@yutira-2025.t5adc.mongodb.net/?retryWrites=true&w=majority";
-    "mongodb+srv://yutira-admin:yHSsegdj6EdTj9q3@cluster0.cthev.mongodb.net/?retryWrites=true&w=majority"
+    "mongodb+srv://yutira-admin:sNrmMV4MO18f9YFT@yutira-2025.t5adc.mongodb.net/?retryWrites=true&w=majority";
+    //"mongodb+srv://yutira-admin:yHSsegdj6EdTj9q3@cluster0.cthev.mongodb.net/?retryWrites=true&w=majority"
 export const connectToServer = (callback) => {
     mongoose
         .connect(connectionString, {})
