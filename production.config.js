c3cdde7ade7e7883a8ff39f09339623c3e3e99bd
Arulkumara Babu <22z310@psgtech.ac.in>
2025-02-05 22:30:47 +0530

    oauth check

--- production.config.js
+++ production.config.js
@@ -1,3 +1,3 @@
-export const BACKEND_URL = "https://yutiradb.psgtech.ac.in";
-export const HELPDESK_URL = "https://kriya-helpdesk.psgtech.ac.in";
-export const FRONTEND_URL = "https://yutira.psgtech.ac.in";
+export const BACKEND_URL = "https://yutira25-backend.vercel.app";
+export const HELPDESK_URL = "cea.civil.psgtech.ac.in";
+export const FRONTEND_URL = "https://yutira.vercel.app";
